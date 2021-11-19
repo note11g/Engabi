9655626668e8fb889abfada269c65c27a0e52a4d
김승빈 <note11@kakao.com>
2021-11-19 23:09:29 +0900

    add : checking AndroidKeyHash in Log (info)

--- app/src/main/java/com/note11/engabi/EngabiApplication.kt
+++ app/src/main/java/com/note11/engabi/EngabiApplication.kt
@@ -1,8 +1,10 @@
 package com.note11.engabi
 
 import android.app.Application
+import android.util.Log
 import com.google.firebase.FirebaseApp
 import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
 
 class EngabiApplication : Application() {
     companion object {
@@ -13,5 +15,8 @@ class EngabiApplication : Application() {
         super.onCreate()
         firebaseApp = FirebaseApp.initializeApp(this)
         KakaoSdk.init(this, "5495cbd8be1677e13b1960ba6a714e75")
+
+        val keyHash = Utility.getKeyHash(this)
+        Log.i("AndroidKeyHash", keyHash)
     }
 }
