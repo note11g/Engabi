f3691ec1784beb00d24d0bbcf1e532c3ee6997ec
skyneton <67633420+skyneton@users.noreply.github.com>
2021-12-19 10:43:17 +0900

    File move to private folder and audio play update

--- app/src/main/java/com/note11/engabi/util/GetFilesUtil.kt
+++ app/src/main/java/com/note11/engabi/util/GetFilesUtil.kt
@@ -1,24 +1,27 @@
 package com.note11.engabi.util
 
 import android.content.Context
+import android.content.Intent
 import android.media.AudioManager
 import android.media.MediaPlayer
+import android.net.Uri
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class GetFilesUtil {
     companion object {
-        fun getFiles(context : Context) = File(context.filesDir.absolutePath).listFiles()
+        fun getFiles(context: Context) = File(context.filesDir.absolutePath).listFiles()
 
-        fun getFileFromPath(path : String) = File(path)
+        fun getFileFromPath(path: String) = File(path)
 
-        fun playAudioFiles(file : File) : MediaPlayer {
-            var mediaPlayer = MediaPlayer()
-            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
-            mediaPlayer.setDataSource(file.absolutePath)
-            mediaPlayer.prepare()
-            mediaPlayer.start()
-
-            return mediaPlayer
+        fun playAudioIntent(context: Context, file: File) : MediaPlayer {
+            val intent = Intent()
+            intent.action = Intent.ACTION_VIEW
+            intent.setDataAndType(Uri.fromFile(file), "audio/*")
+            context.startActivity(intent)
         }
+
+        fun moveFile2PrivateFolder(context: Context, file: File) : Boolean = file.renameTo(File(context.filesDir.absolutePath, file.name))
     }
 }
